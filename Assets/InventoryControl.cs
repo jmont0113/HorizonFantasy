9098612d31b6a69194171cb3adea17045612e406
Jade <jade_montague@yahoo.com>
2021-02-26 01:58:26 -0600

    Control Scheme part 6

---
+++ Assets/InventoryControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryControl : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            GameManager.instance.guiManager.OpenInventory(false);
+            GameManager.instance.SetControlScheme(ControlScheme.Exploration);
+        }
+    }
+}
