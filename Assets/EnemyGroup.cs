f63594b575e433014653ab2b631bb5fcd45e6514
Jade <jade_montague@yahoo.com>
2021-02-27 16:00:23 -0600

    World Map Enemy Encounter

--- Assets/EnemyGroup.cs
+++ Assets/EnemyGroup.cs
@@ -1,18 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyGroup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform spawnPoint;
+    EnemySpawner enemySpawner;
+    NavMeshAgent agent;
+
+    float timer;
+    [SerializeField] float maxTimer = 20f;
+
+    [SerializeField]
+    Transform boxPivot;
+    [SerializeField]
+    Vector3 boxHalfSize = Vector3.one;
+    Transform target;
+
+    private void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        enemySpawner = spawnPoint.GetComponent<EnemySpawner>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        CheckVision();
+
+        if(target == null)
+        {
+            Wandering();
+        }
+        else
+        {
+            Chase();
+        }
+    }
+
+    private void Chase()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    void Wandering()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0f)
+        {
+            timer = maxTimer * Random.value;
+            SetDestination();
+        }
+    }
+
+    void SetDestination()
+    {
+        Vector3 pos = spawnPoint.position;
+        pos += Vector3.right * Random.Range(-enemySpawner.wanderingDistnace, enemySpawner.wanderingDistnace);
+        pos += Vector3.forward * Random.Range(-enemySpawner.wanderingDistnace, enemySpawner.wanderingDistnace);
+        agent.SetDestination(pos);
+    }
+
+    public void CheckVision()
+    {
+        target = null;
+        Collider[] colliders = Physics.OverlapBox(boxPivot.position, boxHalfSize);
+        foreach (Collider c in colliders)
+        {
+            Targetable targetable = c.GetComponent<Targetable>();
+            if (targetable != null)
+            {
+                target = targetable.transform;
+                break;
+            }
+        }
     }
 }
--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -1,10 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-   private void OnDrawGizmos()
+    EnemySpawnerContainer container;
+    public float wanderingDistnace = 5f;
+
+    private void Start()
+    {
+        container = transform.parent.GetComponent<EnemySpawnerContainer>();
+
+        if(container == null) { Debug.LogWarning("No EnemySpawnerContainer has been referenced! EnemySpawner will not spawn enemies!"); }
+
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        if(container == null) { return; }
+
+        //Quaternion.identity
+        GameObject go = Instantiate(container.enemyGroupPrefab, transform.position, Quaternion.Euler(0f, 180f, 0f));
+        go.GetComponent<EnemyGroup>().spawnPoint = transform;
+    }
+
+    private void OnDrawGizmos()
    {
         Gizmos.DrawIcon(transform.position, "EnemySpawner", false);
    }
