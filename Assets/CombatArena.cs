266835f187564f0737885935c2f38ebfcb04af9b
Jade <jade_montague@yahoo.com>
2021-02-28 03:23:09 -0600

    Combat Area part 1

---
+++ Assets/CombatArena.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatArena : MonoBehaviour
+{
+    public List<Transform> characterSpawnPoints;
+    public List<Transform> enemySpawnPoints;
+}
