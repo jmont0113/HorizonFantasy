7e61bc0e4c913dcb4d7d834359c7384af6eedff0
Jade <jade_montague@yahoo.com>
2021-04-23 00:56:18 -0500

    Dialogue

---
+++ Assets/Dialogue/DialogueOptions.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue Option", menuName = "DialoguesOptions")]
+public class DialogueOptions : DialogueBase
+{
+
+}
