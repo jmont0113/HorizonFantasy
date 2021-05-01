b2c33bbec6812047a5c500acd58c07d82dad6ef0
Jade <jade_montague@yahoo.com>
2021-05-01 14:15:07 -0500

    Character

---
+++ Assets/Dialogue/CharacterProfile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Profile", menuName = "Character Profile")]
+public class CharacterProfile : ScriptableObject
+{
+    public string myName;
+    public Sprite myPortrait;
+
+}
